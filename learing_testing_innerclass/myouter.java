0655691c246c5f2cefd8ca799213e615b1981cd0
19mddil <mddilshadul98@gmail.com>
2019-08-08 07:59:15 +0600

    documenting

--- learing_testing_innerclass/myouter.java
+++ learing_testing_innerclass/myouter.java
@@ -1,8 +1,8 @@
 class myouter{
 	private int x;
-	
+	myinner inner;
 	void damn_outer_do_some_stuff(){
-		myinner inner = new myinner();
+		inner = new myinner();//// its actually like myouter outer = new outer(); myouter.myinner inner = outer.new myinner(); like they got a especial bond for both cases declaring inside of a outter class or from a main function of another testing class
 		inner.go();
 	}
 	
