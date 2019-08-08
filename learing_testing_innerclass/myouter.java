1ff59f385acf0997cb3fd8e43b3aada3b05c1686
19mddil <mddilshadul98@gmail.com>
2019-08-08 08:19:46 +0600

    more documentations

--- learing_testing_innerclass/myouter.java
+++ learing_testing_innerclass/myouter.java
@@ -1,15 +1,25 @@
 class myouter{
-	private int x;
-	myinner inner;
+	public int x;
+	myinner inner = new myinner();
+	//myinner.myouter outer = inner.new myouter(); wrong too...
 	void damn_outer_do_some_stuff(){
-		inner = new myinner();//// its actually like myouter outer = new outer(); myouter.myinner inner = outer.new myinner(); like they got a especial bond for both cases declaring inside of a outter class or from a main function of another testing class
+		//inner = new myinner();//// its actually like myouter outer = new outer(); myouter.myinner inner = outer.new myinner(); like they got a especial bond for both cases declaring inside of a outter class or from a main function of another testing class
 		inner.go();
 	}
-	
 	class myinner{
+		int y;
 		void go(){
 			x = 42;
 			System.out.println(x);
+			stealling_his_wife();
 		}
 	}
+	/*intended mistake so we can not do it!!!! vice versa in wrong...*/
+	
+	void stealling_his_wife(){
+		inner.y = 12;
+		System.out.println(inner.y);
+		//inner.go();
+	}
+	
 }
--- learing_testing_innerclass/testing.java
+++ learing_testing_innerclass/testing.java
@@ -1,9 +1,13 @@
 class testing{
 	public static void main(String[] args){
 		myouter outer = new myouter();
-		//myinner inner = new myinner(); ok I can not do it...!!! They must have a especial bond.Its possible only inside of its outter class
-		//myouter.myinner inner = outer.new myinner();
-		//inner.go();
-		outer.damn_outer_do_some_stuff();
+		//myinner inner = new myinner();// ok I can not do it...!!! They must have a especial bond.Its possible only inside of its outter class
+		myouter.myinner inner = outer.new myinner();
+		
+		
+		//inuter.damn_outer_do_some_stuff();
+		outer.stealling_his_wife();
+		//outer.inner.x = 78;
+		//outer.y = 100;
 	}
 }
