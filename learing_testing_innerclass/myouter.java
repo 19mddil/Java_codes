fe13c6c8e5979214021e46e5d490696daa9b7b8f
19mddil <mddilshadul98@gmail.com>
2019-08-08 07:54:07 +0600

    more testing of inner

--- learing_testing_innerclass/myouter.java
+++ learing_testing_innerclass/myouter.java
@@ -1,5 +1,11 @@
 class myouter{
 	private int x;
+	myinner inner = new myinner();
+	
+	void damn_outer_do_some_stuff(){
+		inner.go();
+	}
+	
 	class myinner{
 		void go(){
 			x = 42;
--- learing_testing_innerclass/testing.java
+++ learing_testing_innerclass/testing.java
@@ -1,8 +1,9 @@
 class testing{
 	public static void main(String[] args){
 		myouter outer = new myouter();
-		//myinner inner = new myinner(); ok I can not do it...!!! They must have a especial bond.
-		myouter.myinner inner = outer.new myinner();
-		inner.go();
+		//myinner inner = new myinner(); ok I can not do it...!!! They must have a especial bond.Its possible only inside of its outter class
+		//myouter.myinner inner = outer.new myinner();
+		//inner.go();
+		outer.damn_outer_do_some_stuff();
 	}
 }
