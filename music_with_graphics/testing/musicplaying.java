632070fcf25ec88081d8c6c76699efd8e9290249
19mddil <mddilshadul98@gmail.com>
2019-08-10 08:55:06 +0600

    fixed some issues

--- music_with_graphics/testing/musicplaying.java
+++ music_with_graphics/testing/musicplaying.java
@@ -1,32 +1,49 @@
 import javax.sound.midi.*;
 
 public class musicplaying{
-	void play(){
-		try{
-			
-			
-			Sequencer player = MidiSystem.getSequencer();
-			player.open();
-			
-			Sequence seq = new Sequence(Sequence.PPQ,4);
-			
-			
-			Track track = seq.createTrack();
-			
-			
-			ShortMessage a = new ShortMessage();
-			a.setMessage(144,1,44,100);
-			MidiEvent noteon = new MidiEvent(a,1);
-			track.add(noteon);
-			
-			ShortMessage b = new ShortMessage();
-			b.setMessage(128,1,44,100);
-			MidiEvent noteof = new MidiEvent(a,16);
-			track.add(noteof);
-			
-			
-		}catch(Exception ex){
-			ex.printStackTrace();
-		}
+	void play(int ofbit){
+			try{
+				
+					Sequencer player = MidiSystem.getSequencer();
+					player.open();
+					
+					Sequence seq = new Sequence(Sequence.PPQ,4);
+					
+					
+					Track track = seq.createTrack();
+					
+					
+					ShortMessage a = new ShortMessage();
+					a.setMessage(144,1,44,100);
+					MidiEvent noteon = new MidiEvent(a,1);
+					track.add(noteon);
+					ShortMessage b = new ShortMessage();
+					b.setMessage(128,1,44,100);
+					MidiEvent noteof = new MidiEvent(b,ofbit);
+					track.add(noteof);
+					a = new ShortMessage();
+					a.setMessage(144,1,60,100);
+					noteon = new MidiEvent(a,1);
+					track.add(noteon);
+					b = new ShortMessage();
+					b.setMessage(128,1,60,100);
+					noteof = new MidiEvent(b,ofbit);
+					track.add(noteof);
+					ShortMessage a2 = new ShortMessage();
+					a2.setMessage(144,1,70,100);
+					MidiEvent noteon1 = new MidiEvent(a2,ofbit);
+					track.add(noteon1);
+					ShortMessage b2 = new ShortMessage();
+					b2.setMessage(128,1,70,100);
+					MidiEvent noteof2 = new MidiEvent(b2,ofbit+21);
+					track.add(noteof2);
+					
+					player.setSequence(seq);
+					player.start();
+				
+				
+			}catch(Exception ex){
+				ex.printStackTrace();
+			}
 	}
 }
--- music_with_graphics/testing/testing.java
+++ music_with_graphics/testing/testing.java
@@ -1,6 +1,7 @@
 class testing{
 	public static void main(String[] args){
-		musicplaying mp = new musicplaying();
-		mp.play();
+		
+			musicplaying mp = new musicplaying();
+			mp.play(5);
 	}
 }
