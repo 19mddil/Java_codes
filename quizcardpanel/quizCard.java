22aa906400cafc89935d4b5a0707980dc6ab1bd0
19mddilaccer <mddilshadul98@gmail.com>
2019-08-23 22:05:25 +0600

    in accer

---
+++ quizcardpanel/quizCard.java
@@ -0,0 +1,6 @@
+import java.util.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+
---
+++ quizcardpanel/quizcardBuilder.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+
+class quizcardBuilder{
+	JTextArea question;
+	JTextArea answer;
+	ArrayList<quizCard> cardList;
+	
+}
